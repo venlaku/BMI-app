880b3b99a0c14210f31a1771a0e226a1fb052e4b
VenlaKu <venla.i.kuosmanen@gmail.com>
2021-10-20 18:22:31 +0300

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.practice.ionicreact',
+  appName: 'ionic-react',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
